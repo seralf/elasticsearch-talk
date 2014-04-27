be5d607ea50e8b217195bf0b485560a3bb3bd670
Kevin Greene <kevin@spantree.net>
2014-04-26 19:21:53 -0500

    Making gh-pages from presentation

--- transform/src/main/groovy/net/spantree/elasticsearch/talk/ExampleParts.groovy
+++
@@ -1,6 +0,0 @@
-package net.spantree.elasticsearch.talk
-
-// use these enums as flags to know which part of the example we're traversing
-enum ExampleParts {
-    TITLE, DESCRIPTION, REQUEST, PAYLOAD
-}
--- visualizations/README.md
+++
@@ -1,3 +0,0 @@
-# NFL Elasticsearch Aggregations
-
-This repository contains the files for the data visualization tutorial using Elasticsearch aggregations (1.0 release) and D3.
